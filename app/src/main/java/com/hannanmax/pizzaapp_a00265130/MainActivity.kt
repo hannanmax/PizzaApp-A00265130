c55f2e2a5bc2c6c4bbd08a543cd9bcb5a6ee9647
Hannan Max <48342941+hannanmax@users.noreply.github.com>
2022-11-18 22:26:59 -0500

    Created projected and pushing to github

---
+++ app/src/main/java/com/hannanmax/pizzaapp_a00265130/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hannanmax.pizzaapp_a00265130
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
