47d914aa46d86763df3962ebd079f8419346a5cb
Hannan Max <48342941+hannanmax@users.noreply.github.com>
2022-11-27 20:43:14 -0500

    Added Banner in MainActivity, used firebase realtime for database data and firebase storage for banner image

--- app/src/main/java/com/hannanmax/pizzaapp_a00265130/MainActivity.kt
+++ app/src/main/java/com/hannanmax/pizzaapp_a00265130/MainActivity.kt
@@ -1,8 +1,19 @@
 package com.hannanmax.pizzaapp_a00265130
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.carousel.auna.interfaces.ItemClickListener
+import com.carousel.auna.models.SlideModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import com.hannanmax.pizzaapp_a00265130.Model.Banner
 import com.hannanmax.pizzaapp_a00265130.databinding.ActivityMainBinding
 import com.hannanmax.pizzaapp_a00265130.databinding.ActivitySignUpBinding
 import com.iammert.library.readablebottombar.ReadableBottomBar
@@ -10,6 +21,8 @@ import com.iammert.library.readablebottombar.ReadableBottomBar
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    val database = Firebase.database
+    val myBannersRef = database.getReference("Offers/Banners")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +37,37 @@ class MainActivity : AppCompatActivity() {
                     1 -> Toast.makeText(applicationContext,"1", Toast.LENGTH_LONG).show()
                     2 -> Toast.makeText(applicationContext,"2", Toast.LENGTH_LONG).show()
                 }
+            }
+        })
+
+        // Settings up banners to main screen
+        setupBanners()
+    }
+
+    // Method: setting up banners from firebase realtime database
+    private fun setupBanners() {
+        val imageList = ArrayList<SlideModel>()
+        myBannersRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                imageList.clear()
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.getValue()
+                Log.d(TAG, "Value is: " + value.toString())
+
+                for (ds in snapshot.getChildren()) {
+                    val imageUrl = ds.child("img").getValue(String::class.java)
+                    val imageTitle = ds.child("title").getValue(String::class.java)
+                    if (imageUrl != null && imageTitle != null) {
+                        imageList.add(SlideModel(imageUrl,imageTitle,true))
+                    }
+                }
+                binding.banner.setImageList(imageList) //centerCrop for all images
+                binding.banner.startSliding(3000)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
             }
         })
     }
