e4e316e7d178e0e6f73bfaa9415082cf9bf31898
Hannan Max <48342941+hannanmax@users.noreply.github.com>
2022-12-09 20:06:58 -0500

    Added Database helper class associated with firebase realtime database

---
+++ app/src/main/java/com/hannanmax/pizzaapp_a00265130/Data/FirebaseDBHelper.kt
@@ -0,0 +1,102 @@
+package com.hannanmax.pizzaapp_a00265130.Data
+
+import android.content.ContentValues
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import com.carousel.auna.models.SlideModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class FirebaseDBHelper {
+
+    private val database = Firebase.database
+    private val myBannersRef = database.getReference("Offers/Banners")
+    private val myPizzasRef = database.getReference("Pizza")
+    private val myCartRef = database.getReference("Cart")
+    var pizzaItemArrayList : ArrayList<PizzaItemList> = ArrayList()
+    var pizzaItemNodeArrayList : ArrayList<String> = ArrayList()
+    val bannerList = ArrayList<SlideModel>()
+
+    @JvmName("getPizzaItemArrayList1")
+    fun getPizzaItemArrayList(): Pair<ArrayList<String>,ArrayList<PizzaItemList>>{
+        myPizzasRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    pizzaItemArrayList.clear()
+                    pizzaItemNodeArrayList.clear()
+                    for (ds in snapshot.children) {
+                        for (mDs in ds.children) {
+                            pizzaItemNodeArrayList.add(mDs.key!!)
+                            val pizzaItems = mDs.getValue(PizzaItemList::class.java)
+                            pizzaItemArrayList.add(pizzaItems!!)
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(ContentValues.TAG, "Failed to read value.", error.toException())
+            }
+        })
+        return pizzaItemNodeArrayList to pizzaItemArrayList
+    }
+
+    @JvmName("getBannerList1")
+    fun getBannerList(): ArrayList<SlideModel>{
+        myBannersRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                bannerList.clear()
+                for (ds in snapshot.children) {
+                    val imageUrl = ds.child("img").getValue(String::class.java)
+                    val imageTitle = ds.child("title").getValue(String::class.java)
+                    if (imageUrl != null && imageTitle != null) {
+                        bannerList.add(SlideModel(imageUrl, imageTitle,true))
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(ContentValues.TAG, "Failed to read value.", error.toException())
+            }
+        })
+        return bannerList
+    }
+
+    fun addToCart(context: Context, pizzaID: String, price: String, pizzaSize: String, quantity: Int) {
+        myCartRef.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val currentUser = FirebaseAuth.getInstance().currentUser!!.uid
+                if (snapshot.hasChild("Cart-$currentUser")) {
+                    if (snapshot.hasChild("Cart-$currentUser/item-$pizzaID$pizzaSize")) {
+                        Toast.makeText(context,"Updating current pizza to cart$pizzaSize", Toast.LENGTH_SHORT).show()
+                        myCartRef.child("Cart-$currentUser/item-$pizzaID$pizzaSize").child("pizzaID").setValue(pizzaID)
+                        myCartRef.child("Cart-$currentUser/item-$pizzaID$pizzaSize").child("price").setValue(price)
+                        myCartRef.child("Cart-$currentUser/item-$pizzaID$pizzaSize").child("size").setValue(pizzaSize)
+                        myCartRef.child("Cart-$currentUser/item-$pizzaID$pizzaSize").child("quantity").setValue(quantity)
+                    } else {
+                        Toast.makeText(context,"Adding new pizza to cart$pizzaSize", Toast.LENGTH_SHORT).show()
+                        myCartRef.child("Cart-$currentUser/item-$pizzaID$pizzaSize").child("pizzaID").setValue(pizzaID)
+                        myCartRef.child("Cart-$currentUser/item-$pizzaID$pizzaSize").child("price").setValue(price)
+                        myCartRef.child("Cart-$currentUser/item-$pizzaID$pizzaSize").child("size").setValue(pizzaSize)
+                        myCartRef.child("Cart-$currentUser/item-$pizzaID$pizzaSize").child("quantity").setValue(quantity)
+                    }
+                } else {
+                    Toast.makeText(context,"Adding First pizza to cart$pizzaSize", Toast.LENGTH_SHORT).show()
+                    myCartRef.child("Cart-$currentUser/item-$pizzaID$pizzaSize").child("pizzaID").setValue(pizzaID)
+                    myCartRef.child("Cart-$currentUser/item-$pizzaID$pizzaSize").child("price").setValue(price)
+                    myCartRef.child("Cart-$currentUser/item-$pizzaID$pizzaSize").child("size").setValue(pizzaSize)
+                    myCartRef.child("Cart-$currentUser/item-$pizzaID$pizzaSize").child("quantity").setValue(quantity)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(ContentValues.TAG, "Failed to read value.", error.toException())
+            }
+        })
+    }
+}
