3fe62d4d1963d74df1eef34e71c281c8ac4b4b80
Hannan Max <48342941+hannanmax@users.noreply.github.com>
2022-12-09 20:06:41 -0500

    Created Profile Activity

---
+++ app/src/main/java/com/hannanmax/pizzaapp_a00265130/ProfileActivity.kt
@@ -0,0 +1,29 @@
+package com.hannanmax.pizzaapp_a00265130
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hannanmax.pizzaapp_a00265130.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.llCartbtn.setOnClickListener{
+            val intent = Intent(applicationContext, CartActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
+        binding.llHomebtn.setOnClickListener{
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+    }
+}
