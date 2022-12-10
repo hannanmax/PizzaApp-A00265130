7685ca8c8bfd6c272ba3c5ef636f88b149d5364b
Hannan Max <48342941+hannanmax@users.noreply.github.com>
2022-12-09 20:07:54 -0500

    Created Data model class for Pizza Items List

---
+++ app/src/main/java/com/hannanmax/pizzaapp_a00265130/Data/PizzaItemList.kt
@@ -0,0 +1,3 @@
+package com.hannanmax.pizzaapp_a00265130.Data
+
+data class PizzaItemList(var Name: String ?= null, var Desc: String ?= null, var img: String ?= null, var smPrice: Float ?= null, var mPrice: Float ?= null, var lgPrice: Float ?= null)
