a48e7e27d614ba88493ca167b5c30bde7628c4ba
Hannan Max <48342941+hannanmax@users.noreply.github.com>
2022-12-16 02:05:33 -0500

    Updated cart according to the custom pizza in cart

--- app/src/main/java/com/hannanmax/pizzaapp_a00265130/CartActivity.kt
+++ app/src/main/java/com/hannanmax/pizzaapp_a00265130/CartActivity.kt
@@ -1,16 +1,28 @@
 package com.hannanmax.pizzaapp_a00265130
 
+import android.content.ContentValues
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.hannanmax.pizzaapp_a00265130.Adapter.CustomCartListAdapter
+import com.hannanmax.pizzaapp_a00265130.Data.CartItemList
 import com.hannanmax.pizzaapp_a00265130.Data.FirebaseDBHelper
 import com.hannanmax.pizzaapp_a00265130.databinding.ActivityCartBinding
 
 class CartActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCartBinding
-    private var adapter: CustomCartListAdapter? = null
+    private lateinit var adapter: CustomCartListAdapter
+    var cartTotal:Float = 0f
+    var tax:Float = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +35,20 @@ class CartActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.llOrdersbtn.setOnClickListener{
+            val intent = Intent(applicationContext, PlacedOrdersActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
         binding.llProfilebtn.setOnClickListener{
             val intent = Intent(applicationContext, ProfileActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
+        binding.btnCheckout.setOnClickListener {
+            val intent = Intent(applicationContext, CheckoutActivity::class.java)
             startActivity(intent)
         }
 
@@ -37,7 +60,54 @@ class CartActivity : AppCompatActivity() {
         val firebaseDBHelper = FirebaseDBHelper()
         val (cartItemNodeArrayList, cartItemArrayList) = firebaseDBHelper.getCartItemArrayList(applicationContext)
         val (pizzaItemNodeArrayList, pizzaItemArrayList) = firebaseDBHelper.getPizzaItemArrayList()
-        adapter = CustomCartListAdapter(this, cartItemNodeArrayList, cartItemArrayList, pizzaItemNodeArrayList, pizzaItemArrayList)
-        binding.lwPizzaList.adapter = adapter
+        val myCartRef = Firebase.database.getReference("Cart")
+        myCartRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    cartItemArrayList.clear()
+                    cartItemNodeArrayList.clear()
+                    for (ds in snapshot.children) {
+                        val currentUser = FirebaseAuth.getInstance().currentUser!!.uid
+                        if (snapshot.hasChild("Cart-$currentUser")) {
+                            for (mDs in ds.children) {
+                                cartItemNodeArrayList.add(mDs.key!!)
+                                val cartItems = mDs.getValue(CartItemList::class.java)
+                                cartItemArrayList.add(cartItems!!)
+                            }
+                        } else {
+                            Toast.makeText(applicationContext, "Nothing is in the cart yet...", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    adapter = CustomCartListAdapter(applicationContext, cartItemNodeArrayList, cartItemArrayList, pizzaItemNodeArrayList, pizzaItemArrayList)
+                    binding.lwPizzaList.adapter = adapter
+                    adapter.notifyDataSetChanged()
+                    updateCartCheckOut(cartItemArrayList)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(ContentValues.TAG, "Failed to read value.", error.toException())
+            }
+        })
+    }
+
+    private fun updateCartCheckOut(cartItemArrayList: ArrayList<CartItemList>) {
+        var count = 0
+        Log.d("CartTotalSIZE: ", cartItemArrayList.size.toString())
+        for(item in cartItemArrayList){
+            try {
+                cartTotal += cartItemArrayList.get(count).price!!
+                Log.d("CartTotal: ", item.price!!.toString())
+                count++
+            } catch (e: java.lang.Exception) {
+
+            }
+        }
+
+        tax = String.format("%.02f", (cartTotal*13)/100).toFloat()
+
+        binding.tvSubtotal.text = "Subtotal $" + String.format("%.02f", cartTotal) + " + Tax $$tax"
+        binding.btnCheckout.text = "Proceed to checkout ($count Items)"
+        binding.btnCheckout.isClickable = true
     }
 }
